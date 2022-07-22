d46291f484b2ce8cf5a6bcb63e5668d196dec566
Your New Name <Your new email>
2022-07-22 18:44:38 -0300

    done

---
+++ aula_2/script_aula_2.R
@@ -0,0 +1,46 @@
+#Encontrar valor 2
+conjunto<- c(1, 6, 7, 2, 9)
+
+for (i in 1:length(conjunto)) {
+  if (conjunto[i] == 2) {
+    print(i)
+  } else {
+    next
+  }
+}
+
+#Estrutura sequencial
+x<-2
+y<-x+2
+print(x*y)
+
+#vetores
+x<-seq(1,10,2)
+c<-seq(5,10,length=30)
+y<-c(1,2,3,4,5)
+z<-1:10
+a<-"blablabla"
+b<-c("a","b","c")
+d<-rep(c(0,1,2),times=10)
+
+y[1]
+y[c(1,2)]
+y[1]<-2
+
+ls() #comando para listar os objetos
+rm(a) #remote o objeto a
+rm(list=ls()) #remove tudo
+
+#matrizes
+x<-1:9
+m<-matrix(x,ncol=3,nrow=3)
+m2<-matrix(x,ncol=3,nrow=3,byrow=T)
+m+m2
+t(m^m2)
+m[1,1]
+m[,1]
+m[c(1,2),]
+colSums(m)
+
+matriz<-matrix(c(4,5,-3, 2,1,0, 3,-1,1), ncol=3, nrow=3, byrow=T)
+det(matriz)
